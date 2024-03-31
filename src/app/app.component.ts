8bc7e9eaaa8e804beb1dd6795d474707cef45cd5
Irwin <127706722+Irwinesc@users.noreply.github.com>
2024-03-30 20:13:25 -0600

    fusion between the 2 projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
+import { SliderComponent } from '../components/slider/slider.component';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { PeliculasComponent } from '../components/peliculas/peliculas.component';
+import { PeliculaComponent } from '../components/pelicula/pelicula.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, SliderComponent, SidebarComponent, FooterComponent, PeliculasComponent, PeliculaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blog-app';
+  title = 'angular-proyect';
 }
