91bfbd6a639ed2147941e626120571c6eab82318
Irwin <127706722+Irwinesc@users.noreply.github.com>
2024-05-27 15:35:04 -0600

    Hiding Leo Shark

--- src/services/peliculas.service.ts
+++ src/services/peliculas.service.ts
@@ -11,7 +11,7 @@ export class PeliculaService {
             new Pelicula('Spiderman 4', 2017, 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfsbJjQlmPIQ6v4tHP_m8wlVB6LafjMLGgDmimLmqJ6uuVnpTT'),
             new Pelicula('Los vengadores Endgame', 2019, 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/03/vengadores%202.jpg?tf=3840x'),
             new Pelicula('Batman vs Superman', 2016, 'https://m.media-amazon.com/images/I/81GpJfqOTmL._AC_SY500_.jpg'),
-            new Pelicula('Leo Shark y Giodzilla', 2024, 'https://miweb.com')
+            // new Pelicula('Leo Shark y Giodzilla', 2024, 'https://miweb.com')
             // {year: 2017, title: "Spiderman 4", image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfsbJjQlmPIQ6v4tHP_m8wlVB6LafjMLGgDmimLmqJ6uuVnpTT'},
             // {year: 2019, title: "Los vengadores Endgame", image: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/03/vengadores%202.jpg?tf=3840x'},
             // {year: 2016, title: 'Batman vs Superman', image: 'https://m.media-amazon.com/images/I/81GpJfqOTmL._AC_SY500_.jpg'},
